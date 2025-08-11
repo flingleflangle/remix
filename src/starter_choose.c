bbb4e16f6d2d1a3aa715943db07cbd1af6b5dbef
flingleflangle <eh.notthatitmatters@gmail.com>
2025-08-11 15:12:25 -0700

    new starters, trading with frlg enabled

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -112,9 +112,9 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_TREECKO,
-    SPECIES_TORCHIC,
-    SPECIES_MUDKIP,
+    SPECIES_MAWILE,
+    SPECIES_TORKOAL,
+    SPECIES_NOSEPASS,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
