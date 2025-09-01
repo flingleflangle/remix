c073981fcf209db9f3540c6f9567fa092eb1be7c
flingleflangle <eh.notthatitmatters@gmail.com>
2025-09-01 05:14:37 -0700

    revert to brendan and may, acro bike goes up ledges

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -29,6 +29,8 @@
 #include "constants/mauville_old_man.h"
 #include "constants/trainer_types.h"
 #include "constants/union_room.h"
+#include "constants/metatile_behaviors.h"
+#include "bike.h"
 
 #define SPECIAL_LOCALIDS_START (min(LOCALID_CAMERA, \
                                 min(LOCALID_PLAYER, \
@@ -7672,6 +7674,7 @@ u8 GetLedgeJumpDirection(s16 x, s16 y, u8 direction)
 
     u8 behavior;
     u8 index = direction;
+	struct ObjectEvent *playerObjEvent = &gObjectEvents[gPlayerAvatar.objectEventId];
 
     if (index == DIR_NONE)
         return DIR_NONE;
@@ -7683,6 +7686,14 @@ u8 GetLedgeJumpDirection(s16 x, s16 y, u8 direction)
 
     if (ledgeBehaviorFuncs[index](behavior) == TRUE)
         return index + 1;
+	
+	if (gPlayerAvatar.acroBikeState == ACRO_STATE_BUNNY_HOP &&
+       MB_JUMP_EAST <= behavior && behavior <= MB_JUMP_SOUTH)
+   {
+       MoveCoords(direction, &x, &y);
+       if (GetCollisionAtCoords(playerObjEvent, x, y, direction) == COLLISION_NONE)
+           return index + 1;
+   }
 
     return DIR_NONE;
 }
