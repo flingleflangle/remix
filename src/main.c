3fc48ad7d9eb8d8134f92025b203cdb7e264511c
flingleflangle <eh.notthatitmatters@gmail.com>
2025-09-01 17:55:38 -0700

    soft reset [?] and saving speedup

--- src/main.c
+++ src/main.c
@@ -24,6 +24,11 @@
 #include "main.h"
 #include "trainer_hill.h"
 #include "constants/rgb.h"
+#include "main_menu.h"
+#include "save.h"
+#include "new_game.h"
+
+EWRAM_DATA u8 gSoftResetFlag;
 
 static void VBlankIntr(void);
 static void HBlankIntr(void);
@@ -31,6 +36,7 @@ static void VCountIntr(void);
 static void SerialIntr(void);
 static void IntrDummy(void);
 extern void CB2_FlashNotDetectedScreen(void);
+static void CB2_PostSoftResetInit(void);
 
 const u8 gGameVersion = GAME_VERSION;
 
@@ -92,7 +98,6 @@ void AgbMain(void)
     // Modern compilers are liberal with the stack on entry to this function,
     // so RegisterRamReset may crash if it resets IWRAM.
 #if !MODERN
-    RegisterRamReset(RESET_ALL);
 #endif //MODERN
     *(vu16 *)BG_PLTT = RGB_WHITE; // Set the backdrop to white on startup
     InitGpuRegManager();
@@ -181,7 +186,11 @@ static void InitMainCallbacks(void)
     gTrainerHillVBlankCounter = NULL;
     gMain.vblankCounter2 = 0;
     gMain.callback1 = NULL;
-    SetMainCallback2(CB2_InitCopyrightScreenAfterBootup);
+    if (gSoftResetFlag) {
+		SetMainCallback2(CB2_PostSoftResetInit);
+		} else {
+		SetMainCallback2(CB2_InitCopyrightScreenAfterBootup);
+	}
     gSaveBlock2Ptr = &gSaveblock2.block;
     gPokemonStoragePtr = &gPokemonStorage.block;
 }
@@ -441,4 +450,15 @@ void DoSoftReset(void)
 void ClearPokemonCrySongs(void)
 {
     CpuFill16(0, gPokemonCrySongs, MAX_POKEMON_CRIES * sizeof(struct PokemonCrySong));
+}
+
+static void CB2_PostSoftResetInit(void){
+    SetSaveBlocksPointers(GetSaveBlocksPointersBaseOffset());
+    ResetMenuAndMonGlobals();
+    Save_ResetSaveCounters();
+    LoadGameSave(SAVE_NORMAL);
+    if (gSaveFileStatus == SAVE_STATUS_EMPTY || gSaveFileStatus == SAVE_STATUS_CORRUPT)
+        Sav2_ClearSetDefault();
+    SetPokemonCryStereo(gSaveBlock2Ptr->optionsSound);
+    SetMainCallback2(CB2_InitMainMenu);
 }
