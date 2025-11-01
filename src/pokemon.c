441a6b2c848f2dde5610cac6eb5d9530c7de7819
flingleflangle <eh.notthatitmatters@gmail.com>
2025-11-01 01:11:36 -0700

    marionette messiah

--- src/pokemon.c
+++ src/pokemon.c
@@ -6419,6 +6419,8 @@ u16 GetBattleBGM(void)
 
         switch (trainerClass)
         {
+        case TRAINER_CLASS_RICH_BOY:
+            return MUS_T_MARIONETTE;
         case TRAINER_CLASS_AQUA_LEADER:
         case TRAINER_CLASS_MAGMA_LEADER:
             return MUS_VS_AQUA_MAGMA_LEADER;
